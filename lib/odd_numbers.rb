c6c73bfb4a70e2d91b498b78143a98afa3b50621
rowlandjl <rowlandjl@vcu.edu>
2015-12-18 20:43:25 -0500

    first commit

---
+++ lib/odd_numbers.rb
@@ -0,0 +1,10 @@
+# YOUR CODE HERE
+i = 100
+
+loop do
+  i -= 1
+  if i.odd?
+    puts "#{i}"
+  break if i <= 1
+  end
+end
---
+++ odd_numbers.rb
@@ -0,0 +1,9 @@
+i = 0
+
+loop do
+  i += 1
+  if i.odd?
+    puts "#{i}"
+  break if i >= 99
+  end
+end
